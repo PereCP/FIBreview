d2f3cecc82804b087e2d8be074caad32e1c33d09
Alexandru Dumitru Maroz <alexandru666@outlook.es>
2024-11-07 20:14:14 +0100

    Hot fix

--- src/components/reviewRevision.tsx
+++ src/components/reviewRevision.tsx
@@ -13,16 +13,16 @@ export function ReviewRevision({ review }: ReviewProps): JSX.Element {
   return (
     <div className="flex flex-row gap-2">
       <ReviewComponent review={review} />
-      <div className="prose prose-sm mx-auto bg-white px-6 py-3 shadow sm:rounded-lg">
-        <p>Verdict</p>
-        <Link href="/api/admin/review/accept" passHref>
+      <div className="prose prose-sm mx-auto flex flex-col gap-2 bg-white px-6 py-3 shadow sm:rounded-lg ">
+        <p className="my-2 text-center align-middle">Verdict</p>
+        <Link href={`/api/admin/review/accept?id=${review._id}`} passHref>
           <button className="rounded border border-black bg-white px-4 pb-2 pt-3 text-black transition-colors hover:bg-green-500 hover:text-white">
             <span className="material-symbols-outlined">check</span>
           </button>
         </Link>
-        <Link href="/api/admin/review/reject" passHref>
+        <Link href={`/api/admin/review/reject?id=${review._id}`} passHref>
           <button className="rounded border border-black bg-white px-4 pb-2 pt-3 text-black transition-colors hover:bg-red-500 hover:text-white">
-            <span className="material-symbols-outlined">block</span>
+            <span className="material-symbols-outlined">close</span>
           </button>
         </Link>
       </div>
